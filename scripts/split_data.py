4e039022f403860c60e749179c34b136d2e9d427
HCEhlers <holgerehlers@hotmail.com>
2024-04-03 20:32:58 +0200

    version 1 of split data script

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -1,42 +1,40 @@
-########## split data into train and test folder ##################
-import os
+# Split data into train and validation folder 
 import shutil 
+import os
+from sklearn.model_selection import train_test_split
 
 #Use only this method if running directly from scripts folder otherwise comment out the 
-# following change.
-# os.chdir('..')
+os.chdir('..')
 
 cwd = os.getcwd()
 data_path = cwd
-# Choose boundingbox path appropriately (video1 or video2)
-video = 'video1'
-bb_path = "/data/" + video + "/boundingboxes/"
-data = os.listdir(data_path + bb_path)
 
+# Define path to image data
+path = f"{cwd}/labeled_data/images"
+bb_path = f"{cwd}/labeled_data/labels"
+data = os.listdir(path)
 
-# Split files into train and test
-from sklearn.model_selection import train_test_split
-
+# Split to training and validation
 train, test = train_test_split(data, test_size=0.2, random_state=1)
 
 # Create test and train directory
-train_path = cwd + "/data/" + video + "/train/"
-test_path = cwd + "/data/" + video + "/test/"
-img_path = cwd + "/data/" + video + "/frames/"
-
+train_path = cwd + "/training_data/train/"
+test_path = cwd + "/training_data/test/"
+img_path = cwd + "/training_data/images/"
 
 train_box = train_path + "/boundingboxes"
-train_frames = train_path + "/frames"
+train_images = train_path + "/images"
 test_box = test_path + "/boundingboxes"
-test_frames = test_path + "/frames"
+test_images = test_path + "/images"
+
 
 try:
    os.mkdir(train_path)
    os.mkdir(test_path)
    os.mkdir(train_box)
-   os.mkdir(train_frames)
+   os.mkdir(train_images)
    os.mkdir(test_box)
-   os.mkdir(test_frames)
+   os.mkdir(test_images)
 except FileExistsError:
    pass
 
@@ -44,15 +42,16 @@ except FileExistsError:
 # Move train files to train folder
 for file_name in train:
    #Boundingboxes folder:
-   shutil.copy(os.path.join(data_path + bb_path, file_name), train_box)
+   shutil.copy(os.path.join(path, file_name), train_box)
    #Images folder:
-   shutil.copy(os.path.join(img_path, file_name[:-4] + '.jpg'), train_frames)
+   print(f"{path}/{file_name}")
+   shutil.copy(os.path.join(path, file_name[:-4] + '.jpg'), train_images)
 
 
 # Move test files to test folder
-for file_name in test:
-   shutil.copy(os.path.join(data_path + bb_path, file_name), test_box)
-   shutil.copy(os.path.join(img_path, file_name[:-4] + '.jpg'), test_frames)
+#for file_name in test:
+#   shutil.copy(os.path.join(data_path + bb_path, file_name), test_box)
+#   shutil.copy(os.path.join(img_path, file_name[:-4] + '.jpg'), test_frames)
 
-print("Size train data: ", len(train), "\n","Size test data: ", len(test))
-print(len(data))
+#print("Size train data: ", len(train), "\n","Size test data: ", len(test))
+#print(len(data))
